e7d60e32009a57cde06fc059a486da726aa7c3d2
Jean-Jean Wei <jeanjean.wei@gmail.com>
2021-04-19 23:28:23 +0800

    first commit

---
+++ CodingChallenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CodingChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] { 1, 2, 3, 4, 5 };
+            LeftRotation leftRotation = new LeftRotation();
+            int[] output = leftRotation.Looping(input, 4);
+            for(int i=0; i<output.Length; i++)
+            {
+
+                Console.WriteLine(output[i]);
+            }
+        }
+
+
+    }
+}
