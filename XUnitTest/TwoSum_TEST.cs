1f8e7d2d593873056474a6e64365427d9303c9b7
Jean-Jean Wei <jeanjean.wei@gmail.com>
2021-04-22 15:26:17 +0800

    implement two sum test cases

--- XUnitTest/TwoSum_TEST.cs
+++ XUnitTest/TwoSum_TEST.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using Xunit;
+using CodingChallenge;
 
 namespace XUnitTest
 {
     public class TwoSum_TEST
     {
-        public TwoSum_TEST()
+
+        [Theory, MemberData(nameof(SplitData))]
+        public void TwoSumReturnPositions_TEST(string input, int target, string expected)
         {
+            int[] arr = Array.ConvertAll(input.Split(' '), temp => Convert.ToInt32(temp));
+            int[] exp = Array.ConvertAll(expected.Split(' '), temp => Convert.ToInt32(temp));
+            TwoSum ts = new TwoSum();
+            int[] output = ts.TwoSumReturnPositions(arr, target);
+            Assert.True(Array.Exists(output, x=> x == exp[0]));
+            Assert.True(Array.Exists(output, x=> x == exp[1]));
         }
 
         public static IEnumerable<object[]> SplitData
@@ -16,10 +26,10 @@ namespace XUnitTest
                 // Or this could read from a file. :)
                 return new[]
                 {
-                    new object[] { "2 1 5 3 4", "3"},
-                    new object[] { "2 5 1 3 4", "Too chaotic" },
-                    new object[] { "5 1 2 3 7 8 6 4", "Too chaotic" },
-                    new object[] { "1 2 5 3 7 8 6 4", "7" },
+                    new object[] { "2 1 5 3 4", 6, "1 2"},
+                    new object[] { "1 2 5 3 7 8 6 4", 3,  "0 1" },
+                    new object[] { "3 2 4", 6,  "2 1" },
+                    new object[] { "2 7 11 15", 9,  "1 0" },
                 };
             }
         }
