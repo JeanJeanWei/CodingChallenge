57ad0eb87cf21a319cafe08c1db609ca4482801c
Jean-Jean Wei <jeanjean.wei@gmail.com>
2021-04-25 22:38:13 +0800

    implement BalancedBrackeds and Unittest

---
+++ CodingChallenge/BalancedBrackets.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingChallenge
+{
+    public class BalancedBrackets
+    {
+        public BalancedBrackets()
+        {
+        }
+
+        /*
+         * 1. create a char stack
+         * 2. scann the string elements (char) if stack is empty push the char to stack
+         * 3. if stack is not empty compare the cha to top element of the stack 
+         *    if the top stack is '{', '[', '(' and match the char '}', ']', ')' pop the stack
+         *    else push the char to the stack
+         * 4. finishing scanning the string if the stack is not empty return false (not balanced), else 
+         *    return true (balanced)
+         */
+        public bool Balanced(string s)
+        {
+
+            Stack<char> bb = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (bb.Count <= 0)
+                {
+                    //Console.WriteLine("push " + c);
+                    bb.Push(c);
+                    continue;
+                }
+                char temp = bb.Peek();
+                if (c == '}' && temp == '{')
+                {
+                    //Console.WriteLine("Pop " + temp);
+                    bb.Pop();
+                }
+                else if (c == ']' && temp == '[')
+                {
+                    //Console.WriteLine("Pop " + temp);
+                    bb.Pop();
+                }
+                else if (c == ')' && temp == '(')
+                {
+                    //Console.WriteLine("Pop " + temp);
+                    bb.Pop();
+                }
+                else
+                {
+                    //Console.WriteLine("push " + c);
+                    bb.Push(c);
+                }
+            }
+            Console.WriteLine(bb.Count);
+            return bb.Count == 0 ? true : false;
+        }
+
+    }
+}
---
+++ XUnitTest/BalancedBrackets_TEST.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using CodingChallenge;
+
+namespace XUnitTest
+{
+    public class BalancedBrackets_TEST
+    {
+        [Theory, MemberData(nameof(SplitData))]
+        public void Balanced_TEST(string input, bool expected)
+        { 
+            BalancedBrackets bb = new BalancedBrackets();
+            bool result = bb.Balanced(input);
+            Assert.Equal(result, expected);
+        }
+
+        public static IEnumerable<object[]> SplitData
+        {
+            get
+            {
+                // Or this could read from a file. :)
+                return new[] {
+
+
+
+                    new object[] { "{[()]}", true},
+                    new object[] { "{[(])}", false },
+                    new object[] { "{{[[(())]]}}", true },
+                    new object[] { "{{([])}}", true },
+                    new object[] { "{{)[](}}", false },
+                    new object[] { "{(([])[])[]}", true },
+                    new object[] { "{(([])[])[]]}", false },
+                    new object[] { "{(([])[])[]}[]", true}
+
+                };
+            }
+        }
+    }
+
+    
+}
