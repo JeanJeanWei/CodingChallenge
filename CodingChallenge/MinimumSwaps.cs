a116d556b25930e9201c15691d3afe11f434b801
Jean-Jean Wei <jeanjean.wei@gmail.com>
2021-04-26 22:08:18 +0800

    add MinimunSwaps and test for array size of 100000 (read from file)

---
+++ CodingChallenge/MinimumSwaps.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace CodingChallenge
+{
+    public class MinimumSwaps
+    {
+        public MinimumSwaps()
+        {
+        }
+
+        public int MinSwapsDirect(int[] arr)
+        {
+
+            int count = 0;
+            int i = 0;
+            while (i < arr.Length)
+            {
+                if (arr[i] != i + 1)
+                {
+                    int temp = arr[i];
+                    //Console.WriteLine("i:" + i + " arr[i]:" + arr[i] + " arr[arr[i]-1]:" + arr[arr[i] - 1]);
+                    arr[i] = arr[arr[i] - 1];
+                    arr[temp - 1] = temp;
+                    count++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return count;
+        }
+
+
+        //Time complesity O(nLog(n)+n)
+        public int MinSwapsCompareToOrderedArray(int[] arr)
+        {
+
+            int count = 0;
+            int N = arr.Length;
+            int[] temp = new int[N];
+            // created a consecutive ordered array O(n)
+            for (int i = 0; i < N; i++)
+            {
+                temp[i] = i + 1;
+            }
+
+            //O(nLog(n))
+            for (int i = 0; i < N; i++)
+            {
+
+                // check if the current element is at the place it supposed to be
+                if (arr[i] != temp[i])
+                {
+                    count++;
+
+                    // scan the arry and swap the current element with the index supposed to be
+                    Swap(arr, i, IndexOf(arr, temp[i], i));
+                }
+            }
+            return count;
+        }
+
+        private void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        // search through array skiped the already sorted part
+        private int IndexOf(int[] arr, int ele, int start)
+        {
+            for (int i = start + 1; i < arr.Length; i++)
+            {
+                if (arr[i] == ele)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ XUnitTest/MinimumSwaps_TEST.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+using CodingChallenge;
+
+namespace XUnitTest
+{
+    public class MinimumSwaps_TEST
+    {
+
+        static string txtPath = Path.Combine(Environment.CurrentDirectory, "MinSwapTest1.txt");
+
+        [Theory, MemberData(nameof(SplitData))]
+        public void MinimumSwapsDirect_TEST(string input, string expected)
+        {
+            int[] arr = Array.ConvertAll(input.Split(' '), temp => Convert.ToInt32(temp));
+            MinimumSwaps ms = new MinimumSwaps();
+            int result = ms.MinSwapsDirect(arr);
+
+            Assert.Equal(result.ToString(), expected);
+        }
+
+        [Theory, MemberData(nameof(SplitData))]
+        public void MinimumSwapsCompare_TEST(string input, string expected)
+        {
+            int[] arr = Array.ConvertAll(input.Split(' '), temp => Convert.ToInt32(temp));
+            MinimumSwaps ms = new MinimumSwaps();
+            int result = ms.MinSwapsCompareToOrderedArray(arr);
+            // 7 second for large number 
+            Assert.Equal(result.ToString(), expected);
+        }
+
+        public static IEnumerable<object[]> SplitData
+        {
+            get
+            {
+                string[] testfile1 = File.ReadAllLines(txtPath);
+                return new[]
+                {
+                    new object[] { testfile1[1], testfile1[0]},
+                    new object[] { "4 3 1 2", "3"},
+                    new object[] { "2 3 4 1 5", "3" },
+                    new object[] { "1 3 5 2 4 6 7", "3" }
+
+                };
+            }
+        }
+    }
+}
