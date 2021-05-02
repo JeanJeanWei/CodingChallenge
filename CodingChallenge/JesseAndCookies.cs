1b847e22795d065a368fbb19e47a65e250b097d7
Jean-Jean Wei <jeanjean.wei@gmail.com>
2021-05-02 22:57:12 +0800

    JesseAndCookies & large array size unit test

---
+++ CodingChallenge/JesseAndCookies.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace CodingChallenge
+{
+    /*
+     *  Hackerrank problem: Jesse and Cookies
+     */
+    public class JesseAndCookies
+    {
+        public JesseAndCookies()
+        {
+
+        }
+
+        public int Cookies(int k, int[] A)
+        {
+
+            Array.Sort(A);
+            int i = 0,
+            c = A.Length,
+            i0 = 0,
+            c0 = 0,
+            op = 0;
+            while ((A[i] < k || A[i0] < k) && (c0 - i0 + c - i) > 1)
+            {
+                int s1 = i0 == c0 || A[i] <= A[i0] ? A[i++] : A[i0++];
+                
+                int s2 = i0 == c0 || (i != c && A[i] <= A[i0]) ? A[i++] : A[i0++];
+
+                A[c0++] = s1 + 2 * s2;
+                op++;
+                if (i == c)
+                {
+                    i = i0;
+                    c = c0;
+                    c0 = i0;
+                }
+            }
+
+            return c - i > 1 || A[i] >= k ? op : -1;
+        }
+    }
+}
---
+++ MSTest/JesseAndCookies_TEST.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CodingChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest
+{
+    [TestClass]
+    public class JesseAndCookies_TEST
+    {
+
+        static string txtPath = Path.Combine(Environment.CurrentDirectory, "CookiesData1.txt");
+
+        [DataTestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Property)]
+        public void TestMethod_DirectCookies(string list, string target, string expected)
+        {
+
+            int[] arr1 = Array.ConvertAll(list.Split(' '), temp => Convert.ToInt32(temp));
+            int k = Convert.ToInt32(target);
+            JesseAndCookies jc = new JesseAndCookies();
+            int result = jc.Cookies(k, arr1);
+            Assert.AreEqual(expected, result.ToString());
+        }
+
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                string[] testfile1 = File.ReadAllLines(txtPath);
+                return new[]
+                {
+
+                    new object[] { "1 2 3 9 10 12", "7", "2" },
+                    new object[] { "1 1 1", "10", "-1"},
+                    new object[] { "13 47 74 12 89 74 18 38", "90", "5" },
+                    new object[] { "6214 8543 9266 1150 7498 7209 9398 1529 1032 7384 6784 34 1449 7598 8795 756 7803 4112 298 4967 1261 1724 4272 1100 9373",
+                                    "3581", "7"},
+                    new object[] { testfile1[2], testfile1[1], testfile1[0]},
+
+                };
+            }
+        }
+    }
+}
