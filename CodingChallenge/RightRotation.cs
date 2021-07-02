f63aacd42181bf3b2182815d3c2df72be9cf9098
Jean-Jean Wei <jeanjean.wei@gmail.com>
2021-07-02 23:58:18 +0800

    added right rotation and unit test

---
+++ CodingChallenge/RightRotation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace CodingChallenge
+{
+    public class RightRotation
+    {
+        public RightRotation()
+        {
+        }
+
+        public void Rotate(int[] nums, int k)
+        {
+
+            int n = nums.Length;
+            //int d = k%n; 
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int pos = (i + k) % n;
+                result[pos] = nums[i];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = result[i];
+            }
+        }
+
+        public void RotateUseReverse(int[] nums, int k)
+        {
+
+            int n = nums.Length;
+            
+            k %= n;
+
+            Reverse(nums, 0, n - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, n - 1);
+        }
+
+        private void Reverse(int[] nums, int left, int right)
+        {
+            while (left < right)
+            {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
---
+++ MSTest/Rotation_Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using CodingChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest
+{
+    [TestClass]
+    public class Rotation_Test
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Property)]
+        public void TestMethod_R(string list, int k, string expected)
+        {
+            int[] arr1 = Array.ConvertAll(list.Split(','), temp => Convert.ToInt32(temp));
+            int[] result = Array.ConvertAll(expected.Split(','), temp => Convert.ToInt32(temp));
+            RightRotation rr = new RightRotation();
+            rr.RotateUseReverse(arr1, k);
+            
+            for (int i = 0; i< arr1.Length; i++)
+            {
+                Assert.AreEqual(result[i], arr1[i]);
+            }
+            
+        }
+
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { "1,2,3,4,5,6,7",3, "5,6,7,1,2,3,4"},
+
+                };
+            }
+        }
+    }
+}
